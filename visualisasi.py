f51f895c9cebb9e0032f4ebecd91c60a7f7ba1ec
piupiupipiii <silvynurazkia690@gmail.com>
2025-10-05 18:11:23 +0700

    visualitation data

---
+++ visualisasi.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import pandas as pd
+
+# Baca CSV
+file_path = r"C:\Users\ASUS\Downloads\fire_archive_M-C61_669496.csv"
+df = pd.read_csv(file_path)
+
+# Buat plot peta
+plt.figure(figsize=(10, 6))
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.set_extent([90, 145, -11, 6])  # Fokus ke Indonesia
+
+# Plot titik hotspot
+plt.scatter(df['longitude'], df['latitude'], s=5, c='red', alpha=0.6, transform=ccrs.PlateCarree(), label='Hotspot')
+
+plt.title('Distribution of MODIS Hotspots in Indonesia')
+plt.legend()
+plt.show()
